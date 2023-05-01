a6a22c0618b4cd2ad2bce4302ef734fe31c94cb3
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2023-05-01 15:19:19 -0400

    feat(upload): add new version of upload file, but still if buffer error at final of program execution

--- scripts/upload.cpp
+++ scripts/upload.cpp
@@ -2,71 +2,66 @@
 
 using namespace std;
 
-char* get_next_field(FILE *arquivo, unsigned int field_size, string pattern) {
-    char* field = (char *) malloc(field_size * sizeof(char)); 
-    unsigned int pos = 1;
+unsigned int get_next_field(FILE *arquivo, char field[], string pattern) {
+    unsigned int pos = 0;
+    char c;
     
-    // fgetc(arquivo); // consume o '"' inicial
-    field[0] = (char) fgetc(arquivo);
-    field[1] = (char) fgetc(arquivo);
     while (true) {
-        if (pos > 1) {
-            // cout << field[pos] << '(' << pos << ')' << ' ' << id[pos-1] << '(' << pos-1 << ')' << endl;
-            if ((field[pos] == pattern[1]) && (field[pos-1] == pattern[0])) {
+        c = getc(arquivo);
+        if (c == EOF) return 1;
+
+        field[pos] = c;
+        if (pos > 0) {
+            if ((field[pos-1] == pattern[0]) && (field[pos] == pattern[1])) {
                 field[pos] = '\0';
-                break;
+                return 0;
             }
         }
-        field[++pos] = (char) fgetc(arquivo);
+        pos++;
     }
-    return field;
 }
 
 int main(int argc, char *argv[]){
 
     FILE *arquivo;
     char char_curr, char_prev;
-    char pattern[2];
+    char pattern[2], field[1030];
 
-    arquivo = fopen(argv[1], "r");
+    arquivo = fopen("artigo.csv", "r");
     if (!arquivo) {
         cerr << "Não foi possível abrir o arquivo de entrada!";
         return -1;
     }
 
-    while (!feof(arquivo)) {
-
+    while (true) {
         pattern[0] = 34;
         pattern[1] = 59;
         
-        char* id = get_next_field(arquivo, 10, pattern);
-        char* titulo = get_next_field(arquivo, 302, pattern);
-        char* ano = get_next_field(arquivo, 8, pattern);
-        char* autores = get_next_field(arquivo, 152, pattern);
-        char* citacoes = get_next_field(arquivo, 12, pattern);
-        char* data_hora = get_next_field(arquivo, 21, pattern);
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "ID: " << field << endl;
+
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tTITULO: " << field << endl;
+
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tANO: " << field << endl;
+
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tAUTORES: " << field << endl;
+        
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tCITACOES: " << field << endl;
+        
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tATUALIZACAO: " << field << endl;
+        
         pattern[0] = 13;
         pattern[1] = 10;
-        char* snipet = get_next_field(arquivo, 1030, pattern);
-        
-        cout << "ID: " << id << endl;
-        cout << "\tTITULO: " << titulo << endl;
-        cout << "\tANO: " << ano << endl;
-        cout << "\tAUTORES: " << autores << endl;
-        cout << "\tCITACOES: " << citacoes << endl;
-        cout << "\tATUALIZACAO: " << data_hora << endl;
-        cout << "\tSNIPET: " << snipet << endl << endl;
+        if (get_next_field(arquivo, field, pattern)) break;
+        cout << "\tSNIPET: " << field << endl << endl;
 
-        free(id);
-        free(titulo);
-        free(ano);
-        free(autores);
-        free(citacoes);
-        free(data_hora);
-        free(snipet);
-        // break;
-    }    
-    fclose(arquivo);
+    }
+    // fclose(arquivo);
     return 0;
 }
 
